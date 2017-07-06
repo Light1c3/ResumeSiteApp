bbbaeadbcf7ffc6966be8b609a8fa4711bf92616
kElad <elad.karni@gmail.com>
2017-07-06 14:33:19 -0400

    Made a folder to contain all the contect pages and setup the navigation between all of the pages

--- ResumeApp/ResumeApp/ResumeApp/AppNavigationMaster.xaml.cs
+++ ResumeApp/ResumeApp/ResumeApp/AppNavigationMaster.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ResumeApp.Pages;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -34,9 +35,9 @@ namespace ResumeApp
                 {
                     new AppNavigationMenuItem { Id = 0, Title = "Home", TargetType = typeof (MainPage) },
                     new AppNavigationMenuItem { Id = 1, Title = "About", TargetType = typeof (AboutPage) },
-                    new AppNavigationMenuItem { Id = 2, Title = "Resume" },
-                    new AppNavigationMenuItem { Id = 3, Title = "Portfolio" },
-                    new AppNavigationMenuItem { Id = 4, Title = "Contact" },
+                    new AppNavigationMenuItem { Id = 2, Title = "Resume", TargetType = typeof (ResumePage) },
+                    new AppNavigationMenuItem { Id = 3, Title = "Portfolio", TargetType = typeof (PortfolioPage) },
+                    new AppNavigationMenuItem { Id = 4, Title = "Contact", TargetType = typeof (ContactPage) },
                 });
             }
             public event PropertyChangedEventHandler PropertyChanged;
