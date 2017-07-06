3a360b6d6b4b9aa15beacf5a3a3e9c5e8c7c87fd
kElad <elad.karni@gmail.com>
2017-07-06 14:40:34 -0400

    Finished setting up MainPage buttons to the different content pages

--- ResumeApp/ResumeApp/ResumeApp/MainPage.xaml.cs
+++ ResumeApp/ResumeApp/ResumeApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ResumeApp.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,8 +19,26 @@ namespace ResumeApp
 
         private async void btnAbout_Clicked(object sender, EventArgs e)
         {
-            AppNavigationMenuItem pageItem = new AppNavigationMenuItem { Id = 1, Title = "About", TargetType = typeof(AboutPage) };
-            await Navigation.PushModalAsync(new AppNavigation(pageItem));
+            AppNavigationMenuItem aboutPage = new AppNavigationMenuItem { Id = 1, Title = "About", TargetType = typeof(AboutPage) };
+            await Navigation.PushModalAsync(new AppNavigation(aboutPage));
+        }
+
+        private async void btnResume_Clicked(object sender, EventArgs e)
+        {
+            AppNavigationMenuItem resumePage = new AppNavigationMenuItem { Id = 1, Title = "Resume", TargetType = typeof(ResumePage) };
+            await Navigation.PushModalAsync(new AppNavigation(resumePage));
+        }
+
+        private async void btnPortfolio_Clicked(object sender, EventArgs e)
+        {
+            AppNavigationMenuItem portfolioPage = new AppNavigationMenuItem { Id = 1, Title = "Portfolio", TargetType = typeof(PortfolioPage) };
+            await Navigation.PushModalAsync(new AppNavigation(portfolioPage));
+        }
+
+        private async void btnContact_Clicked(object sender, EventArgs e)
+        {
+            AppNavigationMenuItem contactPage = new AppNavigationMenuItem { Id = 1, Title = "Contact", TargetType = typeof(ContactPage) };
+            await Navigation.PushModalAsync(new AppNavigation(contactPage));
         }
     }
 }
